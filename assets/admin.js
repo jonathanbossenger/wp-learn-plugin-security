6c670adac81762d7b2ee6ed0cc0dd48bcdef1938
Jonathan Bossenger <jonathanbossenger@gmail.com>
2023-01-05 11:03:30 +0200

    Updating ajax

--- assets/admin.js
+++ assets/admin.js
@@ -1,22 +1,27 @@
-jQuery(document).ready(function ($) {
-	console.log('WCJHB admin.js loaded');
-	$('.delete-submission').on('click', function (event){
-		console.log('Delete button clicked');
-		var this_button = $(this);
-		event.preventDefault();
-		var id = this_button.data('id');
-		console.log('Delete submission id ' + id);
-		jQuery.post(
-			wcjhb_ajax.ajax_url,
-			{
-				'action': 'delete_form_submission',
-				'id': id,
-			},
-			function( response ){
-				console.log(response);
-				alert('Form submission deleted');
-				document.location.reload();
+jQuery( document ).ready(
+	function ($) {
+		console.log( 'WP Learn Plugin Security admin.js loaded' );
+		$( '.delete-submission' ).on(
+			'click',
+			function (event) {
+				console.log( 'Delete button clicked' );
+				var this_button = $( this );
+				event.preventDefault();
+				var id = this_button.data( 'id' );
+				console.log( 'Delete submission id ' + id );
+				jQuery.post(
+					wp_learn_ajax.ajax_url,
+					{
+						'action': 'delete_form_submission',
+						'id': id,
+					},
+					function (response) {
+						console.log( response );
+						alert( 'Form submission deleted' );
+						document.location.reload();
+					}
+				);
 			}
 		);
-	});
-});
+	}
+);
